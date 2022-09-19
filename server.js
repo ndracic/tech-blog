1869c98ba1c2ee8b6d929e856a180416331ec2a0
Nerms <Nerms@Nermins-MacBook-Pro.local>
2022-09-18 21:23:32 -0500

    file structure and tech installed

---
+++ server.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const express = require('express');
+
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Mary goes robblin with marigold goblins',
+    cookie: { maxAge: null },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        // connect the Sequelize instance with express and sessions
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}!`));
+});
